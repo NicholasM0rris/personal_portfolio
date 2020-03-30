877cec9b43fe1ccc038e0d8b4ac815060ddf0aab
Nick <nicholas_m0rris@hotmail.com>
2020-03-30 11:21:24 +1000

    html updates

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    body = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.CharField(max_length=100)
 
